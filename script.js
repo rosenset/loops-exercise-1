8694c21e24a8111fc37f9f0842e948e611fd2a3f
rosenset <61257952+rosenset@users.noreply.github.com>
2020-05-06 17:13:04 -0400

    loops exercise

---
+++ script.js
@@ -0,0 +1,33 @@
+for (let i = 10; i > 0; i = i -1){
+    console.log(i)
+}
+
+
+
+
+
+for (let i = 0; i < 11; i = i +1){
+    console.log(i)
+}
+
+
+
+
+let i = 10;
+    while (i>=0){
+    console.log(i);
+    i --;
+}
+
+let i2 = 11;
+do {
+    i2 --;
+    console.log(i2);
+} while (i2>0);
+
+
+
+let i3 = [0,1,2,3,4,5,6,7,8,9,10];
+for (let numbers of i3){
+    console.log(numbers);
+}
